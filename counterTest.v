908d10ff063f1609768df54913a0c50a81d0a464
Josue Rodriguez <josue.a.rodriguez@ttu.edu>
2018-02-05 17:36:49 -0600

    frequency module is finished up next is bcd stuff and test benches

--- counterTest.v
+++ counterTest.v
@@ -1,2 +1,8 @@
 //test bench for frequency counter
 module test()
+reg reset = 1;
+wire countOut;
+timer(reset, clock, countOut);
+
+
+endmodule
--- frequency.v
+++ frequency.v
@@ -1,5 +1,5 @@
 //will calculate frequency and display it to board
-module (signalIn, clock, digit, segment);
+module frequency(signalIn, clock, digit, segment);
 
 output [0:7] segment; //seven segment display
 output [0:7] digit; //eight big combination for digit
@@ -15,5 +15,33 @@ reg[0:23]disp_scanner;
 reg[0:31]gatecounter;
 reg gate;
 wire[31:0] count;
+
+//bcd stuff
+
+
+
+always(@posedge clock)
+begin
+  if(gatecounter == 0)
+  begin
+    gatecounter = d'2500;
+    gate = 0;
+    frequency = count;
+  end
+else
+begin
+  gate = 1;
+end
+
+gatecounter = gatecounter - 1'b1;
+disp_scanner = disp_scanner + 1'b1
+if(disp_scanner=='d5000)
+begin
+  digit = {digit[6:0], digit[7]};
+  digit_scan = digit_scan + 'd1;
+  disp_scanner = 0;
+end
+
+end
 
 endmodule
