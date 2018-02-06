dbc0a6b6604f8cdc91b527ca32373485dbb4fc17
Josue Rodriguez <josue.a.rodriguez@ttu.edu>
2018-02-06 01:09:33 -0600

    Made syntax acceptable for vivado, yet to be tested

--- counter.v
+++ counter.v
@@ -10,16 +10,14 @@ reg[0:31] count = 0; //32 bit variable initialized to 0
 
 always @(posedge clock or negedge reset) //do when clock is positive or reset is negative 
     begin
-      if(!reset){ //if reset is low reset the count
+      if(!reset) //if reset is low reset the count
          begin
           count = 0;
           end
-      }
-      else if(count < 32'hFFFFFFFF){ //if count is less than max sample size increment
+      else if(count < 32'hFFFFFFFF) //if count is less than max sample size increment
           begin
-            count++; //not sure if this c syntax works
+            count = count + 1; //not sure if this c syntax works
           end
-      }
     end
 
 endmodule
--- testFrequency.v
+++ testFrequency.v
@@ -5,14 +5,13 @@ module testFrequency();
     wire[0:7] digit;
 
     initial begin
-        clck = 1'b01; //initialize clock to 1 (this will be gate input)
+        clck = 1; //initialize clock to 1 (this will be gate input)
         sigin = 0; //initialize signal to 0
     end    
     always
         begin
             #10 sigin = ~sigin; //invert signal to create square wave
+            #100 clck = ~clck;
         end
     frequency(sigin, clck, digit, segment);
-    #1000 clck = ~clck;
-    $finish
-    endmodule
+endmodule
