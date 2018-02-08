a30cc7e0344e10d778c0668beb4e6545ad12e76d
Josue Rodriguez <josue.a.rodriguez@ttu.edu>
2018-02-08 04:27:26 -0600

    formatting changes

--- VSCodeFiles/bcd_to_seg.v
+++ VSCodeFiles/bcd_to_seg.v
@@ -1,180 +1,164 @@
 
 module bcd_to_seg (
-    bcd,
-	 segment
+    BCD,
+	 seg
     );
 	
-output [6:0]segment;
-input [3:0]bcd;
-reg [6:0]segment;
-always @(bcd)
-begin
-case(bcd) 
-4'b0000:begin				//0
-segment[0] = 1'b1; //a
-segment[1] = 1'b1; //b
-segment[2] = 1'b1; //c
-segment[3] = 1'b1; //d
-segment[4] = 1'b1; //e
-segment[5] = 1'b1; //f
-segment[6] = 1'b0; //g
-end
-4'b0001:begin				//1
-segment[0] = 1'b0; //a
-segment[1] = 1'b1; //b
-segment[2] = 1'b1; //c
-segment[3] = 1'b0; //d
-segment[4] = 1'b0; //e
-segment[5] = 1'b0; //f
-segment[6] = 1'b0; //g
-end
-4'b0010:begin				//2
-segment[0] = 1'b1; //a
-segment[1] = 1'b1; //b
-segment[2] = 1'b0; //c
-segment[3] = 1'b1; //d
-segment[4] = 1'b1; //e
-segment[5] = 1'b0; //f
-segment[6] = 1'b1; //g
-end
-4'b0011:begin				//3
-segment[0] = 1'b1; //a
-segment[1] = 1'b1; //b
-segment[2] = 1'b1; //c
-segment[3] = 1'b1; //d
-segment[4] = 1'b0; //e
-segment[5] = 1'b0; //f
-segment[6] = 1'b1; //g
-end
-4'b0100:begin				//4
-segment[0] = 1'b0; //a
-segment[1] = 1'b1; //b
-segment[2] = 1'b1; //c
-segment[3] = 1'b0; //d
-segment[4] = 1'b0; //e
-segment[5] = 1'b1; //f
-segment[6] = 1'b1; //g
-end
-4'b0101:begin				//5
-segment[0] = 1'b1; //a
-segment[1] = 1'b0; //b
-segment[2] = 1'b1; //c
-segment[3] = 1'b1; //d
-segment[4] = 1'b0; //e
-segment[5] = 1'b1; //f
-segment[6] = 1'b1; //g
-end
-4'b0110:begin				//6
-segment[0] = 1'b1; //a
-segment[1] = 1'b0; //b
-segment[2] = 1'b1; //c
-segment[3] = 1'b1; //d
-segment[4] = 1'b1; //e
-segment[5] = 1'b1; //f
-segment[6] = 1'b1; //g
-end
-4'b0111:begin				//7
-segment[0] = 1'b1; //a
-segment[1] = 1'b1; //b
-segment[2] = 1'b1; //c
-segment[3] = 1'b0; //d
-segment[4] = 1'b0; //e
-segment[5] = 1'b0; //f
-segment[6] = 1'b0; //g
-end
-4'b1000:begin				//8
-segment[0] = 1'b1; //a
-segment[1] = 1'b1; //b
-segment[2] = 1'b1; //c
-segment[3] = 1'b1; //d
-segment[4] = 1'b1; //e
-segment[5] = 1'b1; //f
-segment[6] = 1'b1; //g
-end
-4'b1001:begin				//9
-segment[0] = 1'b1; //a
-segment[1] = 1'b1; //b
-segment[2] = 1'b1; //c
-segment[3] = 1'b1; //d
-segment[4] = 1'b0; //e
-segment[5] = 1'b1; //f
-segment[6] = 1'b1; //g
-end
-4'b1010:begin				//A
-segment[0] = 1'b1; //a
-segment[1] = 1'b1; //b
-segment[2] = 1'b1; //c
-segment[3] = 1'b0; //d
-segment[4] = 1'b1; //e
-segment[5] = 1'b1; //f
-segment[6] = 1'b1; //g
-end
-4'b1011:begin				//B
-segment[0] = 1'b0; //a
-segment[1] = 1'b0; //b
-segment[2] = 1'b1; //c
-segment[3] = 1'b1; //d
-segment[4] = 1'b1; //e
-segment[5] = 1'b1; //f
-segment[6] = 1'b1; //g
-end
-4'b1100:begin				//C
-segment[0] = 1'b1; //a
-segment[1] = 1'b0; //b
-segment[2] = 1'b0; //c
-segment[3] = 1'b1; //d
-segment[4] = 1'b1; //e
-segment[5] = 1'b1; //f
-segment[6] = 1'b0; //g
-end
-4'b1101:begin				//D
-segment[0] = 1'b0; //a
-segment[1] = 1'b1; //b
-segment[2] = 1'b1; //c
-segment[3] = 1'b1; //d
-segment[4] = 1'b1; //e
-segment[5] = 1'b0; //f
-segment[6] = 1'b1; //g
-end
-4'b1110:begin				//E
-segment[0] = 1'b1; //a
-segment[1] = 1'b0; //b
-segment[2] = 1'b0; //c
-segment[3] = 1'b1; //d
-segment[4] = 1'b1; //e
-segment[5] = 1'b1; //f
-segment[6] = 1'b1; //g
-end
-4'b1111:begin				//F
-segment[0] = 1'b1; //a
-segment[1] = 1'b0; //b
-segment[2] = 1'b0; //c
-segment[3] = 1'b0; //d
-segment[4] = 1'b1; //e
-segment[5] = 1'b1; //f
-segment[6] = 1'b1; //g
-end
+output [6:0]seg;
+input [3:0]BCD; //BCD has a 4 bit range (0-15) so ABCDEF conditions are added despite not being used
+reg [6:0]seg;
+always @(BCD)
+    begin
+        case(BCD) 
+            4'b0000:begin				//0
+                seg[0] = 1'b1; //a
+                seg[1] = 1'b1; //b
+                seg[2] = 1'b1; //c
+                seg[3] = 1'b1; //d
+                seg[4] = 1'b1; //e
+                seg[5] = 1'b1; //f
+                seg[6] = 1'b0; //g
+            end
+            4'b0001:begin				//1
+                seg[0] = 1'b0; //a
+                seg[1] = 1'b1; //b
+                seg[2] = 1'b1; //c
+                seg[3] = 1'b0; //d
+                seg[4] = 1'b0; //e
+                seg[5] = 1'b0; //f
+                seg[6] = 1'b0; //g
+            end
+            4'b0010:begin				//2
+                seg[0] = 1'b1; //a
+                seg[1] = 1'b1; //b
+                seg[2] = 1'b0; //c
+                seg[3] = 1'b1; //d
+                seg[4] = 1'b1; //e
+                seg[5] = 1'b0; //f
+                seg[6] = 1'b1; //g
+            end
+            4'b0011:begin				//3
+                seg[0] = 1'b1; //a
+                seg[1] = 1'b1; //b
+                seg[2] = 1'b1; //c
+                seg[3] = 1'b1; //d
+                seg[4] = 1'b0; //e
+                seg[5] = 1'b0; //f
+                seg[6] = 1'b1; //g
+            end
+            4'b0100:begin				//4
+                seg[0] = 1'b0; //a
+                seg[1] = 1'b1; //b
+                seg[2] = 1'b1; //c
+                seg[3] = 1'b0; //d
+                seg[4] = 1'b0; //e
+                seg[5] = 1'b1; //f
+                seg[6] = 1'b1; //g
+            end
+            4'b0101:begin				//5
+                seg[0] = 1'b1; //a
+                seg[1] = 1'b0; //b
+                seg[2] = 1'b1; //c
+                seg[3] = 1'b1; //d
+                seg[4] = 1'b0; //e
+                seg[5] = 1'b1; //f
+                seg[6] = 1'b1; //g
+            end
+            4'b0110:begin				//6
+                seg[0] = 1'b1; //a
+                seg[1] = 1'b0; //b
+                seg[2] = 1'b1; //c
+                seg[3] = 1'b1; //d
+                seg[4] = 1'b1; //e
+                seg[5] = 1'b1; //f
+                seg[6] = 1'b1; //g
+            end
+            4'b0111:begin				//7
+                seg[0] = 1'b1; //a
+                seg[1] = 1'b1; //b
+                seg[2] = 1'b1; //c
+                seg[3] = 1'b0; //d
+                seg[4] = 1'b0; //e
+                seg[5] = 1'b0; //f
+                seg[6] = 1'b0; //g
+            end
+            4'b1000:begin				//8
+                seg[0] = 1'b1; //a
+                seg[1] = 1'b1; //b
+                seg[2] = 1'b1; //c
+                seg[3] = 1'b1; //d
+                seg[4] = 1'b1; //e
+                seg[5] = 1'b1; //f
+                seg[6] = 1'b1; //g
+            end
+            4'b1001:begin				//9
+                seg[0] = 1'b1; //a
+                seg[1] = 1'b1; //b
+                seg[2] = 1'b1; //c
+                seg[3] = 1'b1; //d
+                seg[4] = 1'b0; //e
+                seg[5] = 1'b1; //f
+                seg[6] = 1'b1; //g
+            end
+            4'b1010:begin				//A
+                seg[0] = 1'b1; //a
+                seg[1] = 1'b1; //b
+                seg[2] = 1'b1; //c
+                seg[3] = 1'b0; //d
+                seg[4] = 1'b1; //e
+                seg[5] = 1'b1; //f
+                seg[6] = 1'b1; //g
+            end
+            4'b1011:begin				//B
+                seg[0] = 1'b0; //a
+                seg[1] = 1'b0; //b
+                seg[2] = 1'b1; //c
+                seg[3] = 1'b1; //d
+                seg[4] = 1'b1; //e
+                seg[5] = 1'b1; //f
+                seg[6] = 1'b1; //g
+            end
+            4'b1100:begin				//C
+                seg[0] = 1'b1; //a
+                seg[1] = 1'b0; //b
+                seg[2] = 1'b0; //c
+                seg[3] = 1'b1; //d
+                seg[4] = 1'b1; //e
+                seg[5] = 1'b1; //f
+                seg[6] = 1'b0; //g
+            end
+            4'b1101:begin				//D
+                seg[0] = 1'b0; //a
+                seg[1] = 1'b1; //b
+                seg[2] = 1'b1; //c
+                seg[3] = 1'b1; //d
+                seg[4] = 1'b1; //e
+                seg[5] = 1'b0; //f
+                seg[6] = 1'b1; //g
+            end
+            4'b1110:begin				//E
+                seg[0] = 1'b1; //a
+                seg[1] = 1'b0; //b
+                seg[2] = 1'b0; //c
+                seg[3] = 1'b1; //d
+                seg[4] = 1'b1; //e
+                seg[5] = 1'b1; //f
+                seg[6] = 1'b1; //g
+            end
+            4'b1111:begin				//F
+                seg[0] = 1'b1; //a
+                seg[1] = 1'b0; //b
+                seg[2] = 1'b0; //c
+                seg[3] = 1'b0; //d
+                seg[4] = 1'b1; //e
+                seg[5] = 1'b1; //f
+                seg[6] = 1'b1; //g
+            end
 
 
-endcase
+        endcase
 
-end
-//assign segment[0]=  ((!bcd[0]) && (!bcd[2]))  || (bcd[0] && bcd[2]) || bcd[1] || bcd[3];
-//assign segment[1]=  (!bcd[2])  || (bcd[0] && bcd[1]) || ((!bcd[0]) && (!bcd[1]));
-//assign segment[2]=  (bcd[0])  || (!bcd[1]) ||  (bcd[2]);
+    end
 
-//assign segment[3]=  ((!bcd[0]) && (!bcd[2])) || ((bcd[1]) && (!bcd[2]))   || (bcd[3]) ||  ((!bcd[0]) && (bcd[1])) || ((bcd[0]) && (!bcd[1]) && (bcd[2]));
-//assign segment[4]=  ((!bcd[0]) && (!bcd[2]))   || ((!bcd[0]) && (bcd[1]));
-//assign segment[5]=  (bcd[3])||((!bcd[0]) && (bcd[2]))   || ((!bcd[0]) && (!bcd[1])) ||((!bcd[1]) && (bcd[2])) ;
-//assign segment[6]=  ((!bcd[0]) && (bcd[2])) || ((!bcd[1]) && (bcd[2])) || (bcd[3]) || ((bcd[1]) && (!bcd[2]))  ;
-/*
- f(a) = !A!C + AC + B + D
-f(b) = !C + AB + !A!B
-f(c) = A + !B + C
-f(d) = !A!C + B!C + D + !AB + A!BC
-f(e) = !A!C + !AB
-f(f) = D +!AC + !A!B + !BC
-f(g) = !AC + !BC + D + B!C
-*/
 
 endmodule
--- VSCodeFiles/counterTest.v
+++ VSCodeFiles/counterTest.v
@@ -2,21 +2,21 @@
 //test bench for frequency counter
 `timescale 1 ms/1ps
 module counterTest();
-reg reset;
-reg clock;
-wire[0:31]countOut;
+  reg reset;
+  reg clock;
+  wire[0:31]countOut;
 
-initial begin
-  reset = 1;
-  clock = 0;
-end
-counter c2(reset, clock, countOut); //look at count ount
-always
-    begin
-      #10 clock = ~clock; //invert every ten milliseconds
-    end
+  initial begin
+    reset = 1;
+    clock = 0;
+  end
+  counter c2(reset, clock, countOut); //look at count ount
+  always
+      begin
+       #10 clock = ~clock; //invert every ten milliseconds
+     end
 
-    always
+   always
       begin
         #1000 reset = ~reset; //sample for one second
       end
